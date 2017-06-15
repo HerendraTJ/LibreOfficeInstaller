689c1dcde5790c208eed465d2c8b800fd5564d6d
HerendraTJ <herendra.kurniawan@gmail.com>
2017-06-15 22:11:42 +0700

    installer for LibreOffice via deb

---
+++ installer.sh
@@ -0,0 +1,62 @@
+clear
+#Installing core databases
+dpkg -i libreoffice5.3-ure_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-core_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-en-us_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-en-us-res_5.3.2.2-2_amd64.deb
+#Installing Program
+dpkg -i libobasis5.3-base_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-calc_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-draw_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-en-us-base_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-en-us-calc_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-en-us-math_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-en-us-writer_5.3.2.2-2_amd64.deb
+#installing Aditional extention
+dpkg -i libobasis5.3-extension-beanshell-script-provider_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-extension-javascript-script-provider_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-extension-mediawiki-publisher_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-extension-nlpsolver_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-extension-pdf-import_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-extension-report-builder_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-filter-data_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-gnome-integration_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-graphicfilter_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-images_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-impress_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-kde-integration_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-pyuno_5.3.2.2-2_amd64.deb
+#Installing Program Core
+dpkg -i libreoffice5.3_5.3.2.2-2_amd64.deb
+#Writer is need to install before installing libobasis5.3-librelogo_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-writer_5.3.2.2-2_amd64.deb
+dpkg -i libreoffice5.3-writer_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-en-us-writer_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-librelogo_5.3.2.2-2_amd64.deb
+#Installing Program
+dpkg -i libobasis5.3-math_5.3.2.2-2_amd64.deb
+#Installing Extention
+dpkg -i libobasis5.3-ogltrans_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-onlineupdate_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-ooofonts_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-ooolinguistic_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-postgresql-sdbc_5.3.2.2-2_amd64.deb
+dpkg -i libobasis5.3-python-script-provider_5.3.2.2-2_amd64.deb
+#Installing Program Core
+dpkg -i libobasis5.3-pyuno_5.3.2.2-2_amd64.deb
+#Installing Extention
+dpkg -i libobasis5.3-xsltfilter_5.3.2.2-2_amd64.deb
+#Installing Program
+dpkg -i libreoffice5.3-base_5.3.2.2-2_amd64.deb
+dpkg -i libreoffice5.3-calc_5.3.2.2-2_amd64.deb
+dpkg -i libreoffice5.3-debian-menus_5.3.2-2_all.deb
+#Installing Languanges
+dpkg -i libreoffice5.3-dict-en_5.3.2.2-2_amd64.deb
+dpkg -i libreoffice5.3-dict-es_5.3.2.2-2_amd64.deb
+dpkg -i libreoffice5.3-dict-fr_5.3.2.2-2_amd64.deb
+#Installing Program
+dpkg -i libreoffice5.3-draw_5.3.2.2-2_amd64.deb
+dpkg -i libreoffice5.3-impress_5.3.2.2-2_amd64.deb
+dpkg -i libreoffice5.3-math_5.3.2.2-2_amd64.deb
+
+# Installlation Sucessful , your program is ready to use
